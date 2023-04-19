15666a9fb485c053179f9249ec77832bddc67e48
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-04-18 21:13:55 -0700

    Add token transfer delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -40,13 +40,22 @@ contract Token {
 				returns (bool success)
 		{
 				require(balanceOf[msg.sender] >= _value);
-				require(_to != address(0));
+				_transfer(msg.sender, _to, _value);
+				return true;
+		}
 
-				balanceOf[msg.sender] -= _value;
+		function _transfer(
+				address _from,
+				address _to,
+				uint256 _value
+		) internal {
+				require(_to != address(0));
+				require(balanceOf[_from] >= _value);
+
+				balanceOf[_from] -= _value;
 				balanceOf[_to] += _value;
 
-				emit Transfer(msg.sender, _to, _value);
-				return true;
+				emit Transfer(_from, _to, _value);
 		}
 
 		function approve(address _spender, uint256 _value)
@@ -58,6 +67,21 @@ contract Token {
 				allowance[msg.sender][_spender] = _value;
 
 				emit Approval(msg.sender, _spender, _value);
+				return true;
+		}
+
+		function transferFrom(
+				address _from,
+				address _to,
+				uint256 _value
+		) public returns (bool success) {
+				require(_value <= balanceOf[_from]);
+				require(_value <= allowance[_from][msg.sender]);
+
+				allowance[_from][msg.sender] -= _value;
+
+				_transfer(_from, _to, _value);
+
 				return true;
 		}
 }
