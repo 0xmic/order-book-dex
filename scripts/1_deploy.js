232e5530615d65e0143807c3b365e4be931a78be
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-04-19 21:20:14 -0700

    Add Seed Exchange script

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,12 +1,34 @@
 const hre = require('hardhat')
 
 async function main() {
+  console.log(`Preparing deployment...\n`)
+
+  // Fetch contracts to deploy
   const Token = await hre.ethers.getContractFactory('Token')
-  const token = await Token.deploy()
+  const Exchange = await hre.ethers.getContractFactory('Exchange')
 
-  await token.deployed()
+  // Fetch accounts
+  const accounts = await hre.ethers.getSigners()
+  console.log(
+    `Accounts fetched:\n${accounts[0].address}\n${accounts[1].address}\n`
+  )
 
-  console.log(`Token deployed to ${token.address}`)
+  // Deploy contracts
+  const dappToken = await Token.deploy('Dapp Token', 'DAPP', '1000000')
+  await dappToken.deployed()
+  console.log(`Dapp Token (DAPP) deployed to ${dappToken.address}`)
+
+  const mETH = await Token.deploy('mETH', 'mETH', '1000000')
+  await mETH.deployed()
+  console.log(`mETH deployed to ${mETH.address}`)
+
+  const mDAI = await Token.deploy('mDAI', 'mDAI', '1000000')
+  await mDAI.deployed()
+  console.log(`mDAI deployed to ${mDAI.address}`)
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10)
+  await exchange.deployed()
+  console.log(`Exchange deployed to ${exchange.address}`)
 }
 
 main().catch((error) => {
