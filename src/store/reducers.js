9010cde0fc6e23c6983dc4e95d75846095ad6fb9
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-04-21 13:28:15 -0700

    Add Markets component

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -35,8 +35,8 @@ export const tokens = (
       return {
         ...state,
         loaded: true,
-        contracts: [...state.contracts, action.token],
-        symbols: [...state.symbols, action.symbol]
+        contracts: [action.token],
+        symbols: [action.symbol]
       }
     case 'TOKEN_2_LOADED':
       return {
